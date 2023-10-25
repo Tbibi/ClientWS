cd28c9a3971c7376abe15fc54c21c03bef3a2c2e
Tbibi <ahmedtbibi63@gmail.com>
2023-10-25 19:30:02 +0100

    client

---
+++ src/main/java/org/sid/CLientWS.java
@@ -0,0 +1,15 @@
+package org.sid;
+
+import proxy.BanqueService;
+import proxy.BanqueWS;
+import proxy.Compte;
+
+public class CLientWS {
+    public static void main(String[]args){
+        BanqueService stub =new BanqueWS().getBanqueServicePort();
+        System.out.println(stub.convert(7600));
+        Compte cp=stub.getCompte(5);
+        System.out.println(cp.getCode());
+        System.out.println(cp.getSolde());
+    }
+}
